f4533d6e8107824b9c5bc3999da38f8821e6d157
akajs <akavuejs@gmail.com>
2024-06-07 11:00:40 +0100

    tt

---
+++ vue-pre-training-step-7-routing/src/db/mainDB.js
@@ -0,0 +1,11 @@
+export const mainDB = {
+    heroBackgroundImage: 'https://via.placeholder.com/1600x900',
+    headerNavLinks: [
+      { name: 'Accueil', href: '/', current: true },
+      { name: 'À propos', href: '/about', current: false },
+      { name: 'Utilisateurs', href: '/user', current: false },
+      { name: 'Produits', href: '/produits', current: false },
+      { name: 'Contact', href: '/contact', current: false },
+    ]
+  };
+  
---
+++ vue-pre-training-step-7-routing/src/views/ProductDetail.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Détails du Produit</h1>
+        <p>ID du Produit: {{ productId }}</p>
+        <!-- Afficher plus de détails sur le produit ici -->
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'ProductDetail',
+    data() {
+        return {
+            productId: null
+        };
+    },
+    created() {
+        this.productId = this.$route.params.id; // Accéder au paramètre 'id' de la route
+    }
+}
+</script>
